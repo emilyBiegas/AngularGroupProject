1866c1eb4d0b72fd340c4ca46f16ea2f224db975
Emily Biegas <emilybiegas@gmail.com>
2019-11-15 14:51:22 -0500

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FavoritesComponent,
+    SearchCriteriaComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule,
