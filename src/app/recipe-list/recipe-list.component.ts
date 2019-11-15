060fd5805f2d1f0f7b939226f00656c0f3076a06
Dewanlb <dewanok@gmail.com>
2019-11-15 15:49:17 -0500

    Forgot to save

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { FoodService } from "../services/food.service";
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  recipes: any[];
+  constructor(private foodService: FoodService) {}
+  getSearch(topic: string) {
+    this.foodService.getRecipe(topic).subscribe(data => {
+      this.recipes = data.hits;
+      console.log(this.recipes);
+    });
   }
-
+  ngOnInit() {}
 }
