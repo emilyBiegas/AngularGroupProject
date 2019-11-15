80ffdec30375d56df0288c27b2c20972633486c1
Dewanlb <dewanok@gmail.com>
2019-11-15 15:16:56 -0500

    Set up service

--- src/app/services/food.service.ts
+++ src/app/services/food.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+const API_KEY = "8de79bded75c396c973708b8ee39711a";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class FoodService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  getRecipe(recipe: string): Observable<any> {
+    return this.http.get(
+      `https://api.edamam.com/api/nutrition-details?app_id=`,
+      {
+        params: {
+          q: recipe,
+          apiKey: API_KEY,
+          language: "en"
+        }
+      }
+    );
+  }
 }
