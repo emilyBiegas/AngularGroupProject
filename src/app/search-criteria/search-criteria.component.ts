d4410ca4fe8f06dc4a0d9cfa1408d8b44615bb65
Emily Biegas <emilybiegas@gmail.com>
2019-11-18 09:45:06 -0500

    commit

--- src/app/search-criteria/search-criteria.component.ts
+++ src/app/search-criteria/search-criteria.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { FoodService } from "../services/food.service";
 @Component({
-  selector: 'app-search-criteria',
-  templateUrl: './search-criteria.component.html',
-  styleUrls: ['./search-criteria.component.css']
+  selector: "app-search-criteria",
+  templateUrl: "./search-criteria.component.html",
+  styleUrls: ["./search-criteria.component.css"]
 })
 export class SearchCriteriaComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  recipes: any[];
+  doSearch(criteria: string) {
+    this.foodService.getRecipe(criteria).subscribe(data => {
+      this.recipes = data.hits;
+      console.log(this.recipes);
+    });
   }
+  constructor(private foodService: FoodService) {}
 
+  ngOnInit() {}
 }
--- src/app/services/food.service.ts
+++ src/app/services/food.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { SearchCriteriaComponent } from "../search-criteria/search-criteria.component";
 
 const API_KEY = "a30e2cfbb73fc3dea624656c24d5e8ee";
 
@@ -11,14 +10,11 @@ const API_KEY = "a30e2cfbb73fc3dea624656c24d5e8ee";
 export class FoodService {
   constructor(private http: HttpClient) {}
   getRecipe(criteria: string): Observable<any> {
-    return this.http.get(
-      `http://api.edamam.com/search?q=chicken&app_id=2f9c222e&app_key=${API_KEY}`,
-      {
-        params: {
-          q: criteria,
-          apiKey: API_KEY
-        }
+    return this.http.get(`http://api.edamam.com/search?n&app_id=2f9c222e`, {
+      params: {
+        q: criteria,
+        apiKey: API_KEY
       }
-    );
+    });
   }
 }
